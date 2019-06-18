ef90b64049787a68e289f4fedd7a227d44aa2dc2
Andrea Bellandi <andrea.bellandi@gmail.com>
2019-06-18 15:33:30 +0200

    empty lexicon committed

--- src/main/java/it/cnr/ilc/lexolite/constant/Namespace.java
+++ src/main/java/it/cnr/ilc/lexolite/constant/Namespace.java
@@ -20,7 +20,7 @@ public class Namespace {
     public static final String LIME = "http://www.w3.org/ns/lemon/lime#";
     public static final String DECOMP = "http://www.w3.org/ns/lemon/decomp#";
     public static final String DCT = "http://purl.org/dc/terms/";
-    public static final String LEXINFO = "http://ditmao.ilc.cnr.it:8082/DitmaoOntologies/lexinfo.owl#";
+    public static final String LEXINFO = "https://www.lexinfo.net/ontology/2.0/lexinfo#";
     public static final String RDFS = "http://www.w3.org/2000/01/rdf-schema#";
     public static final String RDF = "http://www.w3.org/1999/02/22-rdf-syntax-ns#";
     public static final String SKOS = "http://www.w3.org/2004/02/skos/core#";
