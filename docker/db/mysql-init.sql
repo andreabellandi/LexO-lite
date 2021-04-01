c41127fa133fa304c78e6d7ab16bfd8d65b1d944
Michele Mallia <32638550+MicheleMallia@users.noreply.github.com>
2021-04-01 11:26:47 +0200

    update mysql-init.sql

--- docker/db/mysql-init.sql
+++ docker/db/mysql-init.sql
@@ -1,6 +1,4 @@
-CREATE DATABASE LexO_users CHARACTER SET utf8 COLLATE utf8_bin;
-
-use LexO_users;
+CREATE DATABASE IF NOT EXISTS LexO_users CHARACTER SET utf8 COLLATE utf8_bin;
 
 CREATE USER 'admin'@'localhost' IDENTIFIED BY 'admin';
 GRANT ALL PRIVILEGES ON LexO_users.* TO 'admin'@'localhost';
